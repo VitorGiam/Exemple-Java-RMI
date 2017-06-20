20c23e22af53b3695f931092066b6414aef4a9dd
VitorGiammella <vitor.giammella@soaexpert.com>
2017-06-20 11:03:23 -0300

    Initial commit.

---
+++ AppCliente.java
@@ -0,0 +1,14 @@
+import java.rmi.*;
+
+public class AppCliente {
+	public static void main(String args[]) {
+		try {
+			// acesso o Binder pelo nome que fora registrado
+			Servidor m = (Servidor) Naming.lookup("rmi://localhost:1099/Servidor");
+			// método utilizado normalmente
+			System.out.println(m.montaMensagem("Ola!"));
+    	} catch (Exception e) {
+      		System.out.println("GenericException: " + e.toString());
+    	}
+	}
+}
---
+++ AppServidor.java
@@ -0,0 +1,15 @@
+import java.rmi.Naming;
+
+public class AppServidor {
+
+    public static void main(String[] args) {
+    	try {
+        	Servidor m = new ServidorImpl();
+        	
+        	// crio uma instancia do obj e registro no Binder
+            Naming.rebind("rmi://localhost:1099/Servidor", m);
+        } catch( Exception e ) {
+        	System.out.println( "Trouble: " + e.getMessage() );
+        }
+    }
+}
